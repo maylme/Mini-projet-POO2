f7d9408bc1767312d5b5aacc83cab17901bd505c
jerem21 <jer_du83@voila.fr>
2013-11-30 16:23:05 +0100

    Update Utilisateurs.java

--- src/utilisateurs/Utilisateurs.java
+++ src/utilisateurs/Utilisateurs.java
@@ -1,5 +1,29 @@
 package utilisateurs;
 
-public abstract class Utilisateurs {
+/**
+ * 
+ * @author Jérémy ROUMEGUE 
+ *
+ */
 
+public abstract class Utilisateurs {
+	
+	private int numIdentifiant;
+	
+	public Utilisateurs(int numIdentifiant) {
+		this.numIdentifiant = numIdentifiant;
+	}
+	
+	public int getNumIdentifiant() {
+		return numIdentifiant;
+	}
+	
+	public void setNumIdentifiant(int numIdentifiant) {
+		this.numIdentifiant = numIdentifiant;
+	}
+	
+	@Override
+	public String toString() {
+		return "L'utilisateurs ayant l'identifiant N°" + numIdentifiant + ", est un" + this.getClass().getSimpleName() + "]";
+	}
 }
