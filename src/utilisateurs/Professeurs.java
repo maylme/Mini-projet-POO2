5da209c59005f9467f765dff0968573a78eb9f9c
jerem21 <jer_du83@voila.fr>
2013-11-30 16:22:35 +0100

    Update Professeurs.java

--- src/utilisateurs/Professeurs.java
+++ src/utilisateurs/Professeurs.java
@@ -1,5 +1,22 @@
 package utilisateurs;
 
-public class Professeurs extends Utilisateurs {
+import gestion.IHM;
+import java.util.GregorianCalendar;
+import materiel.Materiel;
 
+/**
+ * 
+ * @author rj105647
+ *
+ */
+
+public class Professeurs extends Emprunteur {
+
+	public Professeurs(int numIdentifiant) {
+		super(numIdentifiant,15);
+	}
+	
+	public void reserver(Materiel materiel, GregorianCalendar dateEmprunt, GregorianCalendar dateRetour){
+		IHM.reserver(this,materiel,dateEmprunt,dateRetour);
+	}
 }
